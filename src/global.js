efff252275278b207e21bbdb9ea1f8d148ee7f8e
JavierReivajGomez <jafg84alejandro@gmail.com>
2021-04-12 21:10:37 -0500

    Aqui subimos los archivos

---
+++ src/global.js
@@ -0,0 +1,35 @@
+var hello = 'hello';
+var hello = 'hello +';
+let world = 'hello world';
+const helloworld = 'hello world!';
+
+
+const anotherfuncion = () => {
+    console.log(hello);
+    console.log(world);
+    console.log(helloworld);
+}
+
+
+anotherfuncion();
+
+
+const helloworld = () => {
+    globalvar = 'i am global'
+}
+
+
+helloworld();
+console.log(globalvar)
+
+
+
+
+const helloWorld = () =>  {
+    const hello = "hello world"
+    console.log (hello);
+
+};
+
+
+helloWorld();
---
+++ src/local.js
@@ -0,0 +1,22 @@
+const helloworld = () => {
+    const helloWorld = 'hello word';
+    conosole.log(helloWorld);
+};
+
+
+helloWorld();
+console.log(hello);
+
+var scope = "i am global";
+
+
+const functioScope = () => {
+    var scope = "i am just a local";
+    const func = () => {
+        return scrope
+    }
+    console.log(func());
+}
+
+
+functioScope();
